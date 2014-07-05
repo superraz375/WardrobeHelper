7a9702cd3a4eb10d2a89be5e79b6100e766d50a4
Raz Friman <superraz375@yahoo.com>
2014-07-05 16:28:46 -0500

    Initial support for Generating random outfits that matches.

--- app.js
+++ app.js
@@ -113,8 +113,84 @@ app.controller('AllClothesController', function ($scope, $http, $log, $filter, $
 app.controller('RandomOutfitController', function ($scope, $http, $log, $filter, $modal, filterFilter) {
 
     $http.get('clothes.json').success(function(data) {
-        $scope.myData = data;
+        $scope.outfit_data = data;
+
+        $scope.selectRandomOutfit();
     });
+
+    $scope.outfit = {
+        shirt: {},
+        pants: {},
+        shoes: {}
+    };
+
+    $scope.selectRandomOutfit=  function() {
+
+        $scope.outfit_data = $scope.shuffle($scope.outfit_data);
+
+        for(var i = 0; i < $scope.outfit_data.length; i++) {
+            var item = $scope.outfit_data[i];
+
+            if(item.image_id.length > 0) {
+
+                if(item.type == 'Shirt') {
+                    $scope.outfit.shirt = item
+                    break;
+                }
+            }
+        }
+
+        for(var i = 0; i < $scope.outfit_data.length; i++) {
+            var item = $scope.outfit_data[i];
+
+            if(item.image_id.length > 0) {
+                if(item.type == 'Pants') {
+
+                    if(item.primary_color == 'White' ||
+                        item.primary_color == 'Khaki') {
+                        if(item.primary_color != $scope.outfit.shirt.primary_color) {
+                            $scope.outfit.pants = item;
+                            break;
+                        }
+                    }
+
+                    if(item.primary_color == $scope.outfit.shirt.secondary_color) {
+                        $scope.outfit.pants = item;
+                        break;
+                    }
+
+
+
+
+
+                }
+            }
+        }
+
+        for(var i = 0; i < $scope.outfit_data.length; i++) {
+            var item = $scope.outfit_data[i];
+
+            if(item.image_id.length > 0) {
+                if(item.type == 'Shoes') {
+
+                    if(item.detailed_type == 'Loafers') {
+                        $scope.outfit.shoes = item;
+                    }
+                    break;
+                }
+            }
+        }
+
+
+    };
+
+    $scope.shuffle = function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+
+
+
 
     // pick random shirt.
 
